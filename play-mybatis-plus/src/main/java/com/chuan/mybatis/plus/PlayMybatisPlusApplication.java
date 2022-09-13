61733295a030bd6e7aa48bb5fc7ec4b6563ad5a5
chuan <xucy-e@glodon.com>
2022-09-13 14:26:22 +0800

    项目与模块结构重组

--- play-mybatis-plus/src/main/java/com/chuan/mybatis/plus/PlayMybatisPlusApplication.java
+++ play-mybatis-plus/src/main/java/com/chuan/mybatis/plus/PlayMybatisPlusApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.chuan.play.mybatis.plus.dao")
+@MapperScan("com.chuan.mybatis.plus.dao")
 @SpringBootApplication
 public class PlayMybatisPlusApplication {
 
--- play-mybatis-plus/src/main/java/com/chuan/mybatis/plus/config/CreateAndUpdateTimeHandler.java
+++ play-mybatis-plus/src/main/java/com/chuan/mybatis/plus/config/CreateAndUpdateTimeHandler.java
@@ -1,7 +1,6 @@
 package com.chuan.mybatis.plus.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +9,16 @@ import java.time.LocalDateTime;
 /**
  * @author xucy-e
  */
-@Slf4j
 @Component
 public class CreateAndUpdateTimeHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        log.info("start insert fill ....");
         this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.info("start update fill ....");
         this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
     }
 }
